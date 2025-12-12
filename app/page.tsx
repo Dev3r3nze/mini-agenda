d7a0b4808a0aebde0befa39f92af4d50fa956906
dev3r3nze <dev3r3nze@gmail.com>
2025-12-12 22:16:03 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+export const dynamic = "force-dynamic";
+
 import React, { useEffect, useState } from "react";
 import { initFirebase, subscribeToAuthChanges, User } from "../lib/firebase";
 import Sidebar from "../components/sidebar"; // Tu componente Sidebar
